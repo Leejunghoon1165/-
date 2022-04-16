95bad11b258062fedc100b2a0176e8e2513d2d90
MorrinoahJin <71023609+MorrinoahJin@users.noreply.github.com>
2022-04-16 13:44:13 +0900

    aa

--- CardAndDungeon/Assets/Scripts/Mori/Mushroom_Attack.cs
+++ CardAndDungeon/Assets/Scripts/Mori/Mushroom_Attack.cs
@@ -7,9 +7,13 @@ public class Mushroom_Attack : MonoBehaviour
     public Animator anim;
     float time;
     bool bombCount;
+    public Transform pos;
+    public Vector2 boxSize;
+    int strengh;
     void Awake()
     {
         anim = GetComponent<Animator>();
+        strengh = GameObject.Find("MushRoom").GetComponent<MoveManager>().Strengh;
         bombCount = false;
     }
     
@@ -26,15 +30,31 @@ public class Mushroom_Attack : MonoBehaviour
         Bomb();
     }
 
-
-
     void Bomb()
     {    
         if(time > 2f) {
             GameObject.Find("MushRoom").GetComponent<MoveManager>().longRange = true;
-
+            Attack();
             anim.SetTrigger("Attack");
             Destroy(gameObject, 1.5f);
         }
+    }
+
+    void Attack()
+    {
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+        foreach (Collider2D collider in collider2Ds)
+        {
+            /*
+            if(collider.gameObject.tag=="Player")
+                collider.GetComponent<Player>().TakeDamage(Strengh);
+            */
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(pos.position, boxSize);
     }
 }
