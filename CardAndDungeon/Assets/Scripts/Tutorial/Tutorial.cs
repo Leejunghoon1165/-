9dcc82cf687420e7fa59cd1b1aa877393629ef8b
MorrinoahJin <71023609+MorrinoahJin@users.noreply.github.com>
2022-05-22 11:01:55 +0900

    튜토리얼 UI 제작중

--- CardAndDungeon/Assets/Scripts/Tutorial/Tutorial.cs
+++ CardAndDungeon/Assets/Scripts/Tutorial/Tutorial.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public GameObject a, b, c;
+    int count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        OnSingleTouch();
+        /*
+        if(Input.touchCount > 0 ){
+            count += 1;
+        }
+        */
+        if(count == 1){
+            a.SetActive(true);
+            b.SetActive(false);
+            c.SetActive(false);
+        }
+        if(count == 2){
+            a.SetActive(false);
+            b.SetActive(true);
+            c.SetActive(false);
+        }
+        if(count == 3){
+            a.SetActive(false);
+            b.SetActive(false);
+            c.SetActive(true);
+        }
+
+        /*
         if( Input.touchCount == 1){
             Debug.Log("첫번째");
             a.SetActive(true);
@@ -32,5 +56,20 @@ public class Tutorial : MonoBehaviour
             b.SetActive(false);
             c.SetActive(true);
         }
+        */
+
+
+    }
+
+    void OnSingleTouch()
+    {
+        if(Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Ended) {
+                count += 1;
+            }
+        }
+
     }
 }
